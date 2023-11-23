36ed729a52f1e96d5a9c8956901bb7564d853e80
thminggg <tsehongming21@gmail.com>
2023-11-23 00:25:17 -0800

    feat: start working on the ETL UI

--- packages/react/src/app/page.tsx
+++ packages/react/src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { produce } from "../services/kafka";
+import Image from "next/image";
+import producerImg from "../public/factory.png";
+import cargoImg from "../public/cargo.jpg";
 
 export default function Page() {
   async function handleProduce() {
@@ -9,8 +12,25 @@ export default function Page() {
 
   return (
     <>
-      <h1>Hello, Next.js!</h1>
-      <button onClick={handleProduce}>produce</button>
+      <div className="container">
+        <div className="item">
+          <Image
+            src={producerImg}
+            alt="Producer"
+            className="producer"
+            onClick={handleProduce}
+          />
+          <p>Producer</p>
+        </div>
+        <div className="item cargoContainer">
+          <Image
+            src={cargoImg}
+            alt="Cargo"
+            className="cargoImg"
+            onClick={handleProduce}
+          />
+        </div>
+      </div>
     </>
   );
 }
