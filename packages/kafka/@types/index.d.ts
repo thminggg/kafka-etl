971180a3b54332ccbf8d47ad127ad067e7068379
thminggg <tsehongming21@gmail.com>
2024-04-11 00:12:55 -0700

    chore: update kafka index.d.ts

--- packages/kafka/@types/index.d.ts
+++ packages/kafka/@types/index.d.ts
@@ -1,7 +1,3 @@
-declare module "@thminggg/kafka" {
-  const createTopic: () => void;
-  const consume: () => void;
-  const produce: () => void;
-
-  export { createTopic, consume, produce };
-}
+export { createTopic } from "../src/create-topic";
+export { consume } from "../src/consumer";
+export { produce } from "../src/producer";
